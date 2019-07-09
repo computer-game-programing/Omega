f4c991467e126742253b082a02e65a0a779977ef
zhangyuqi <zhangyuqi@fabu.ai>
2019-07-09 10:39:48 +0800

    Adjust Effects

---
+++ 100 Best Effect Pack/Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+
+    private float time_count;
+    // Use this for initialization
+    void Start()
+    {
+        time_count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time_count += Time.deltaTime;
+        if (time_count > 1.1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
